7e468980cf0dfcaef5f82fc5597a81a657099d81
Josemaria666 <jgarciah2306@alumno.ipn.mx>
2025-10-18 21:18:57 -0600

    Realizamos el ejercicio 2

--- EJERCICIOS/EJERCICIOS PARTE 2/funcionesdos.js
+++ EJERCICIOS/EJERCICIOS PARTE 2/funcionesdos.js
@@ -46,6 +46,69 @@ function operacion(){
 
 
 
+function validarn1(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function validarn2(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function validarn3(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function comparacion(){
+    var valor1 = document.getElementById("numuno").value;
+
+    var valor2 = document.getElementById("numdos").value;
+
+    var valor3 = document.getElementById("numtres").value;
+
+
+    var parse1= parseFloat(valor1);
+    alert(parse1);
+
+    var parse2 = parseFloat(valor2)
+    alert(parse2);
+
+    var parse3 = parseFloat(valor3)
+    alert(parse3);
+    
+    if(parse1 > parse2 && parse1 > parse3){
+        var comparar = parse1
+        alert(comparar)    
+    }
+    else{
+        if(parse2> parse1 && parse2 > parse3){
+        var comparar = parse2
+        alert(comparar)    
+        }
+        else{
+        if(parse3 > parse1 && parse3 > parse2){
+        var comparar = parse3
+        alert(comparar)    
+        }      
+        }
+    document.getElementById("comparari").value = comparar;
+    }
+}
+
 function borrari(){
     document.getElementById("saldoi").value = "";
     document.getElementById("cantidadi").value = "";
